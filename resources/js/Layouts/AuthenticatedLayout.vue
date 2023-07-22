60646b5807ba24cf4b8cc1ad9f0b3bb56cf44cac
yuta higa <h.yuta2314@gmail.com>
2023-07-22 15:31:20 +0900

    Changed Nav design.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -12,51 +12,58 @@ const showingNavigationDropdown = ref(false);
 
 <template>
     <div>
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            <div class="flex flex-between min-h-screen bg-gray-100 dark:bg-gray-900">
-                <nav class="flex flex-col overflow-y-hidden w-1/6 px-3 border-r-2 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+        <div class="min-h-screen bg-gray-100">
+            <div class="flex flex-between min-h-screen bg-gray-100">
 
-                    <!-- Logo -->
-                    <div class="shrink-0 flex items-center pt-5">
-                        <Link :href="route('dashboard')">
-                            <ApplicationLogo
-                                class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200 mb-5"
-                            />
-                        </Link>
-                    </div>
-
+                <nav class="flex flex-col overflow-y-hidden rounded-r-3xl border-none w-30 px-3 bg-white">
                     <!-- nav -->
-                    <div class="flex flex-col gap-96">
+                    <div class="flex flex-col gap-20">
                         <!-- menu -->
-                        <div class="flex flex-col p-4">
+                        <div class="p-4">
                             <div class="flex flex-col items-center">
-                                <NavLink class="font-bold text-xl tracking-tight m-2" :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
+                                <!-- Logo -->
+                                <div class="pt-5">
+                                    <Link :href="route('dashboard')">
+                                        <ApplicationLogo
+                                            class="block h-9 w-auto fill-current text-gray-800 mb-5"
+                                        />
+                                    </Link>
+                                </div>
+                                <NavLink class="flex flex-col text-xl tracking-tight m-2" :href="route('dashboard')" :active="route().current('dashboard')">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 512 512"><path d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zm64 64V416H224V160H64zm384 0H288V416H448V160z"/></svg>
+                                    <small class="text-sm font-bold">dashboard</small>
                                 </NavLink>
-                                <NavLink class="font-bold text-xl tracking-tight m-2" :href="route('items.index')" :active="route().current('items.*')">
-                                    商品一覧
+                                <NavLink class="flex flex-col text-xl tracking-tight m-2" :href="route('items.index')" :active="route().current('items.*')">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 640 512"><path d="M36.8 192H603.2c20.3 0 36.8-16.5 36.8-36.8c0-7.3-2.2-14.4-6.2-20.4L558.2 21.4C549.3 8 534.4 0 518.3 0H121.7c-16 0-31 8-39.9 21.4L6.2 134.7c-4 6.1-6.2 13.2-6.2 20.4C0 175.5 16.5 192 36.8 192zM64 224V384v80c0 26.5 21.5 48 48 48H336c26.5 0 48-21.5 48-48V384 224H320V384H128V224H64zm448 0V480c0 17.7 14.3 32 32 32s32-14.3 32-32V224H512z"/></svg>
+                                    <small class="text-sm font-bold">Items</small>
                                 </NavLink>
-                                <NavLink class="font-bold text-xl tracking-tight m-2" :href="route('customers.index')" :active="route().current('customers.*')">
-                                    顧客管理
+                                <NavLink class="flex flex-col text-xl tracking-tight m-2" :href="route('customers.index')" :active="route().current('customers.*')">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 640 512"><path d="M72 88a56 56 0 1 1 112 0A56 56 0 1 1 72 88zM64 245.7C54 256.9 48 271.8 48 288s6 31.1 16 42.3V245.7zm144.4-49.3C178.7 222.7 160 261.2 160 304c0 34.3 12 65.8 32 90.5V416c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V389.2C26.2 371.2 0 332.7 0 288c0-61.9 50.1-112 112-112h32c24 0 46.2 7.5 64.4 20.3zM448 416V394.5c20-24.7 32-56.2 32-90.5c0-42.8-18.7-81.3-48.4-107.7C449.8 183.5 472 176 496 176h32c61.9 0 112 50.1 112 112c0 44.7-26.2 83.2-64 101.2V416c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32zm8-328a56 56 0 1 1 112 0A56 56 0 1 1 456 88zM576 245.7v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM320 32a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM240 304c0 16.2 6 31 16 42.3V261.7c-10 11.3-16 26.1-16 42.3zm144-42.3v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM448 304c0 44.7-26.2 83.2-64 101.2V448c0 17.7-14.3 32-32 32H288c-17.7 0-32-14.3-32-32V405.2c-37.8-18-64-56.5-64-101.2c0-61.9 50.1-112 112-112h32c61.9 0 112 50.1 112 112z"/></svg>
+                                    <small class="text-sm font-bold">Customers</small>
                                 </NavLink>
-                                <NavLink class="font-bold text-xl tracking-tight m-2">
-                                    購入履歴
+                                <NavLink class="flex flex-col text-xl tracking-tight m-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+                                    <small class="text-sm font-bold">Purchase</small>
                                 </NavLink>
-                                <NavLink class="font-bold text-xl tracking-tight m-2">
-                                    データ分析
+                                <NavLink class="flex flex-col text-xl tracking-tight m-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="1.2em" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zm-312 8v64c0 13.3 10.7 24 24 24s24-10.7 24-24l0-64c0-13.3-10.7-24-24-24s-24 10.7-24 24zm80-96V280c0 13.3 10.7 24 24 24s24-10.7 24-24V120c0-13.3-10.7-24-24-24s-24 10.7-24 24zm80 64v96c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
+                                    <small class="text-sm font-bold">Analysis</small>
                                 </NavLink>
+                                <div class="flex flex-col border-t-4">
+                                    <NavLink class="font-bold text-lg tracking-tight m-2" :href="route('profile.edit')">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+                                    </NavLink>
+                                    <NavLink class="font-bold text-lg tracking-tight m-2" :href="route('logout')" method="post" as="button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"/></svg>
+                                    </NavLink>
+                                </div>
                             </div>
                         </div>
 
-                        <!-- profile -->
-                        <div class="flex flex-col p-2 mx-2 bg-white dark:bg-gray-700 rounded-xl">
-                                <NavLink class="font-bold text-lg tracking-tight m-2" :href="route('profile.edit')">Profile</NavLink>
-                                <NavLink class="font-bold text-lg tracking-tight m-2" :href="route('logout')" method="post" as="button">Log Out</NavLink>
-                        </div>
                     </div>
                 </nav>
 
-                <main class="w-5/6 p-5 h-screen overflow-y-scroll">
+                <main class="w-full p-5 h-screen overflow-y-scroll">
                     <slot />
                 </main>
             </div>
