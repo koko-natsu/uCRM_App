63013effd6da266e1ab6d27c7ecceb6cadb51a63
yuta higa <h.yuta2314@gmail.com>
2024-01-08 11:30:14 +0900

    Modified: CustomerResources.

--- app/Http/Controllers/RetrieveCustomerController.php
+++ app/Http/Controllers/RetrieveCustomerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Customer as ResourcesCustomer;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 
@@ -11,34 +12,6 @@ class RetrieveCustomerController extends Controller
     {
         $customer = Customer::findOrFail($request->customer_id);
 
-        list($l_name, $f_name) = preg_split("/\s/", $customer->name);
-        list($l_kana, $f_kana) = preg_split("/\s/", $customer->kana);
-
-        return [
-            'data' => [
-                'type' => 'customers',
-                'customer_id' => $customer->id,
-                'attributes' => [
-                    'name' => [
-                        'first_name' => $f_name,
-                        'last_name'  => $l_name,
-                    ],
-                    'kana' => [
-                        'first_name' => $f_kana,
-                        'last_name'  => $l_kana,
-                    ],
-                    'tel' => $customer->tel,
-                    'email' => $customer->email,
-                    'postcode' => $customer->postcode,
-                    'address' => $customer->address,
-                    'birthday' => $customer->birthday,
-                    'gender' => $customer->gender,
-                    'memo' => $customer->memo,
-                ]
-            ],
-            'links' => [
-                'self' => url('/customers/' .$customer->id)
-            ]
-        ];
+        return new ResourcesCustomer($customer);
     }
 }
--- app/Http/Resources/PurchaseResource.php
+++ app/Http/Resources/PurchaseResource.php
@@ -6,7 +6,7 @@ use App\Http\Resources\Customer as CustomerResource;
 use App\Http\Resources\ReceiptCollection;
 use App\Models\Customer;
 use App\Models\Purchase;
-use Carbon\Carbon;
+use App\Http\Traits\Date;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -15,31 +15,40 @@ class PurchaseResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        $date = $this->excerptDate($this->created_at);
+        $date = Date::excerptDate($this->created_at);
+        $customer = Customer::find($this->customer_id);
 
         return [
             'data' => [
                 'type' => 'purchases',
-                'purchase_id' => $this->purchase_id ?? $this->id,
-                'attributes' => [
-                    'status' => $this->status,
-                    'total'  => (int)$this->total,
+                'purchase_id'      => $this->purchase_id ?? $this->id,
+                'attributes'       => [
+                    'status'       => $this->status,
+                    'total'        => (int)$this->total,
                     'purchase_day' => $date,
-                    'customer' => new CustomerResource(Customer::find($this->customer_id)),
-                    'receipt' => new ReceiptCollection($this->findReceipt($this->id ?? $this->purchase_id)),
+                    // The only data required Purchase is
+                    // customer_id and customer_name, customer_kana.
+                    // 'customer'     => new CustomerResource(Customer::find($this->customer_id)),
+                    'customer' => [
+                        'data' => [
+                            'type' => 'customers',
+                            'customer_id' => $customer->id,
+                            'attributes'  => [
+                                'name' => $customer->name,
+                                'kana' => $customer->kana,
+                            ]
+                        ],
+                        'links' => [
+                            'self' => url('/customers/'. $customer->id),
+                        ]
+                    ],
+                    'receipt'      => new ReceiptCollection($this->findReceipt($this->id ?? $this->purchase_id)),
                 ]
             ],
             'links' => [
                 'self' => url('/purchases/' . ($this->purchase_id ?? $this->id)),
             ]
         ];
-    }
-
-    private function excerptDate(Carbon $date): string
-    {
-        list($excerpt_date, $_) = preg_split('/ /', $date);
-
-        return $excerpt_date;
     }
 
     private function findReceipt(int $purchase_id): Collection
